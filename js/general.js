8338edbfc3fd5ad06fd1191a7e1f45ddddd675f9
PBalazs0517 <peter.balazs0517@gmail.com>
2022-04-25 22:43:51 +0200

    reworked the buttons and now there are 4 different types of tools

--- js/general.js
+++ js/general.js
@@ -1,91 +1,151 @@
-//cubes
-let cubes = document.querySelectorAll(".cubes");
+//general values
+let size = "16"
+let color = "red"
+let bgColor = "rgb(85, 83, 83)";
+let randomColor = "";
+const resizeLabel = document.getElementById("resizelabel");
 
-//input values
-let color = "red";
-let size = 16;
-let heightAndWidth = "";
-
-//change input values
-    //change color
-    let colorPicker = document.getElementById("colorpicker");
-    colorPicker.addEventListener("change", () => {
-        color = colorPicker.value
-    })
-
-    //change size
-    let popup = document.getElementById("popup");
-    let ok = document.getElementById("ok");
-    let resizeButton = document.getElementById("resize");
-    resizeButton.addEventListener("click", () => {
-        popup.style.display = "flex";
-    });
-    ok.addEventListener("click", () => {
-            popup.style.display = "none";
-            deleteCubes();
-            newSize()
-            clacHW();
-            createCubes();
-            cubes = document.querySelectorAll(".cubes");
-            cubes.forEach(cube => { cube.addEventListener("mouseover", e => {
-                e.target.style.cssText = `background-color: ${color}; height: ${heightAndWidth}; width: ${heightAndWidth};`
-            })});
-    })
-        //deletes existing cubes
-        function deleteCubes() {
-            for(let i = 1; i < size * size + 1; i++) {
-                let cube = document.getElementById(`cube${i}`);
-                mainCont.removeChild(cube);
-            }
-        }
+//inputs
+    //mainrow
+        //clear
+        let clear = document.getElementById("clear");
+        clear.addEventListener("click", () => {
+            let cubes = document.querySelectorAll(".cubes");
+            cubes.forEach(cube => {
+                cube.style.cssText += `background-color: ${bgColor}`
+            })
+        });
 
         //resize
-        function newSize(){
-            size = document.getElementById("newsize").value;
-            return size; 
-        }
+        let resize = document.getElementById("resize");
+        resize.addEventListener("change", () => {
+            deleteGrid();
+            size = resize.value;
+            resizeLabel.textContent = `${size} X ${size}`
+            createGrid();
+            return size;
+        });
 
-    //reset cube backgrounds
-    let clear = document.getElementById("clear");
-    clear.addEventListener("click", () => {
-        cubes.forEach(cube => {
-            clacHW();
-            cube.style.cssText = `background-color: rgb(85, 83, 83); height: ${heightAndWidth}; width: ${heightAndWidth};`;
+        //colorpicker
+        let colorpicker = document.getElementById("colorpicker");
+        colorpicker.addEventListener("change", () => {
+            color = colorpicker.value;
+        });
+    
+    //toolsrow
+    let pencilSelected = true;
+    let rubberSelected = false;
+    let fillSelected = false;
+    let rainbowSelected = false;
+        //pencil
+        let pencil = document.getElementById("pencil");
+        pencil.style.cssText = "border: 3px rgb(29, 36, 248) solid;";
+        pencil.addEventListener("click", () => {
+            pencilSelected = true;
+            rubberSelected = false;
+            fillSelected = false;
+            rainbowSelected = false;
+            pencil.style.cssText = "border: 3px rgb(29, 36, 248) solid;";
+            rubber.style.cssText = "border: 3px black solid;";
+            fill.style.cssText = "border: 3px black solid;";
+            rainbow.style.cssText = "border: 3px black solid;";
+            color = colorpicker.value;
         })
-    })
 
-//body
-const body = document.querySelector("#body");
+        //rubber
+        let rubber = document.getElementById("rubber");
+        rubber.addEventListener("click", () => {
+            pencilSelected = false;
+            rubberSelected = true;
+            fillSelected = false;
+            rainbowSelected = false;
+            rubber.style.cssText = "border: 3px rgb(29, 36, 248) solid;";
+            pencil.style.cssText = "border: 3px black solid;";
+            fill.style.cssText = "border: 3px black solid;";
+            rainbow.style.cssText = "border: 3px black solid;";
+            color = bgColor;
+        })
+        //fill
+        let fill = document.getElementById("fill");
+        fill.addEventListener("click", () => {
+            pencilSelected = false;
+            rubberSelected = false;
+            fillSelected = true;
+            rainbowSelected = false;
+            fill.style.cssText = "border: 3px rgb(29, 36, 248) solid;";
+            rubber.style.cssText = "border: 3px black solid;";
+            pencil.style.cssText = "border: 3px black solid;";
+            rainbow.style.cssText = "border: 3px black solid;";
+            color = colorpicker.value;
+            let cubes = document.querySelectorAll(".cubes");
+            cubes.forEach(cube => {
+                cube.style.cssText += `background-color: ${color}`
+            });
+        })
 
-//container where the grid goes
-const mainCont = document.createElement("div");
+        //rainbow
+        let rainbow = document.getElementById("rainbow");
+        rainbow.addEventListener("click", () => {
+            pencilSelected = false;
+            rubberSelected = false;
+            fillSelected = false;
+            rainbowSelected = true;
+            rainbow.style.cssText = "border: 3px rgb(29, 36, 248) solid;";
+            rubber.style.cssText = "border: 3px black solid;";
+            fill.style.cssText = "border: 3px black solid;";
+            pencil.style.cssText = "border: 3px black solid;";
+            console.log(randomColor);
+            color = randomColor;
+        })
+            //random color
+            function randomiseColor() {
+                let colorR = Math.floor(Math.random() * 255);
+                let colorG = Math.floor(Math.random() * 255);
+                let colorB = Math.floor(Math.random() * 255);
+                randomColor = `rgb(${colorR}, ${colorG}, ${colorB})`;
+                return randomColor;
+            }
+
+//body 
+let body = document.querySelector("body")
+
+//div where the grid goes
+let mainCont = document.createElement("div");
 mainCont.id = "mainCont";
+mainCont.style.cssText += `background-color: ${bgColor}`;
 body.appendChild(mainCont);
 
-//calc height and width
-function clacHW(){
-    heightAndWidth = 960/size + "px";
+//cube's height and width
+let heightAndWidth = "";
+function calcHW() {
+    heightAndWidth = 800/size+"px";
     return heightAndWidth;
-};
+}
 
-clacHW();
-
-//grid
-function createCubes(){
-    for(let i = 1; i < size * size + 1; i++) {
+//create grid
+function createGrid() {
+    for(let i = 1; i < size*size+1; i++){
         let cube = document.createElement("div");
         cube.id = `cube${i}`;
-        cube.style.height = heightAndWidth;
-        cube.style.width = heightAndWidth;
         cube.classList.add("cubes");
-        mainCont.appendChild(cube);
-    };
+        calcHW();
+        cube.style.cssText = `height: ${heightAndWidth}; width: ${heightAndWidth};`;
+        cube.addEventListener("mouseover", e => {
+            randomiseColor();
+            if(rainbowSelected == true){
+                e.target.style.cssText += `background-color: ${randomColor};`
+            } else { e.target.style.cssText += `background-color: ${color};`}
+        })
+        mainCont.appendChild(cube)
+    }
 };
 
-createCubes();
+createGrid();
 
-//change color on hover 
-cubes = document.querySelectorAll(".cubes");
-cubes.forEach(cube => { cube.addEventListener("mouseover", e => {
-    e.target.style.cssText = `background-color: ${color}; height: ${heightAndWidth}; width: ${heightAndWidth};`
-})});
+//deletes existing grid 
+function deleteGrid() {
+    for(let i = 1; i < size*size+1; i++){
+        let cube = document.getElementById(`cube${i}`);
+        mainCont.removeChild(cube);
+    }
+}
